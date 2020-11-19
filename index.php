c9cde92b2934cbb8085af8243796ef803548c6c4
Zigi <zigimane@gmail.com>
2020-11-19 13:00:40 +0300

    Testing one two :)

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="inter.css" />
+</head>
+<body>
+<div class="nav">
+	
+</div>
+
+<!-- <div class="cart">
+
+	<p>WHere are you</p>
+	
+</div> -->
+	<div class="container">
+		<header>
+			
+		</header>
+		<div class="rest">
+
+			<div class="face">
+
+				<div class="cats">
+
+				<!-- 	<p>Can you see me</p> -->
+					<div class="pic1">
+						
+					</div>
+					
+				</div>
+
+				<div class="dogs">
+
+					<div class="pic2">
+						
+					</div>
+					<div class="pic2">
+						
+					</div>
+					<div class="pic2">
+						
+					</div>
+					<div class="pic2">
+						
+					</div>
+					<div class="pic2">
+						
+					</div>
+					
+				</div>
+
+				<div class="don">
+
+					<div class="btn">
+						
+					</div>
+					
+				</div>
+
+			</div>
+		</div>
+		<footer>
+
+		<p>© 2020.</p>
+			
+		</footer>
+	</div>
+</body>
+</html>
